821c98d196d461a8fec7c6c6456bb9896311a3da
Thea2 <1822947065@qq.com>
2018-08-07 17:01:58 +0800

    第二次提交

--- calender.py
+++ calender.py
@@ -21,7 +21,7 @@ def first_day(source):
     return (len(blank) + 1) % 7
 
 
-def get_month_dict(source, cur_days, cur_week, year, month):
+def get_month_dict(source, cur_days, cur_week, year, month, last_month_day=0, last_month_week = 0):
     """
     获取这个月的日历
     :param source: 源代码
@@ -36,17 +36,31 @@ def get_month_dict(source, cur_days, cur_week, year, month):
     print "days: ", len(days)
     xiu_pattern = re.compile('wnrl_riqi_xiu')
     ban_pattern = re.compile('wnrl_riqi_ban')
+    holiday_pattern_one = re.compile('<span class="wnrl_td_bzl wnrl_td_bzl_lv">(.*?)</span>')
+    holiday_pattern_two = re.compile('<span class="wnrl_td_bzl wnrl_td_bzl_hong">(.*?)</span>')
     month_dict = {}
 
     day_num = 1
     for day in days:
         day_dict = {}
+        # 是否是节日，如果是保存其节日名称，否则保存-1
+        holiday = re.findall(holiday_pattern_one, day)
+        if len(holiday) != 0:
+            day_dict['traditional_holiday'] = holiday[0]
+        else:
+            holiday = re.findall(holiday_pattern_two, day)
+            if len(holiday) != 0:
+                day_dict['traditional_holiday'] = holiday[0]
+            else:
+                day_dict['traditional_holiday'] = -1
         # 是否是工作日
         ban = len(re.findall(ban_pattern, day))
         if ban != 0:
             day_dict['is_workday'] = 1
         else:
-            if cur_day == 0 | cur_day == 6:
+            if cur_day == 0:
+                day_dict['is_workday'] = 0
+            elif cur_day == 6:
                 day_dict['is_workday'] = 0
             else:
                 day_dict['is_workday'] = 1
@@ -60,10 +74,9 @@ def get_month_dict(source, cur_days, cur_week, year, month):
         # 周几
         day_dict['day'] = cur_day
         # 第几天
-        day_dict['sum_day'] = cur_days
+        day_dict['sum_day'] = cur_days - last_month_day + 1
         # 第几周
-        day_dict['sum_week'] = cur_week
-
+        day_dict['sum_week'] = cur_week - last_month_week + 1
         # 日期
         if day_num < 10:
             day_num = '0' + str(day_num)
@@ -94,10 +107,11 @@ def count_week_holiday(year_dict):
             holiday_num += 1
         if day['day'] == 0:
             for j in range(i - 6, i + 1):
-                if j > 0 & j <= len(year_dict):
-                    year_dict[j]['week_holiday'] = holiday_num
+                if j > 0:
+                    if j <= len(year_dict):
+                        year_dict[j]['week_holiday'] = holiday_num
             holiday_num = 0
-    # 若当年最后一天不是星期天，统计这个不完整的星期的数据
+    # 若最后一天不是星期天，统计这个不完整的星期的数据
     if year_dict[len(year_dict)]['day'] != 0:
         day = year_dict[len(year_dict)]
         for i in range(len(year_dict) - day['day'] + 1, len(year_dict) + 1):
@@ -112,25 +126,56 @@ def count_next_workday(year_dict):
     :return:
     """
     for i in range(1, len(year_dict) + 1):
-        next_day = 1
-        for j in range(i + 1, len(year_dict) + 1):
-            if year_dict[j]['is_workday'] == 1:
-                break
+        if year_dict[i]['is_workday'] == 1:
+            if i < len(year_dict):
+                if year_dict[i + 1]['is_workday'] == 1:
+                    # 若当天为工作日且下一天为工作日时赋值0
+                    year_dict[i]['next_workday'] = 0
+                else:
+                    next_day = 1
+                    for j in range(i + 1, len(year_dict) + 1):
+                        if year_dict[j]['is_workday'] == 1:
+                            break
+                        else:
+                            next_day += 1
+                    year_dict[i]['next_workday'] = next_day
             else:
-                next_day += 1
-        day = year_dict[i]
-        day['next_workday'] = next_day
-        year_dict[i] = day
+                year_dict[i]['next_workday'] = 1
+        else:
+            next_day = 1
+            for j in range(i + 1, len(year_dict) + 1):
+                if year_dict[j]['is_workday'] == 1:
+                    break
+                else:
+                    next_day += 1
+            day = year_dict[i]
+            day['next_workday'] = next_day
+            year_dict[i] = day
     return year_dict
 
 
-def my_main():
-    spider.get_page("https://wannianrili.51240.com/")
-    year = "2017"
-    spider.select_year_option(year)
+def my_main(year_need):
+    """
+    需要爬取某一年的日历
+    :param year_need: int类型,暂支持区间(1900,2100)
+    :return:
+    """
     sum_day = 1
     sum_week = 1
     year_dict = {}
+    # 打开在线日历网页
+    spider.get_page("https://wannianrili.51240.com/")
+
+    # 跳转到需要获取那一年的前一年的12月，并获取数据
+    spider.select_year_option(str(year_need - 1))
+    spider.select_month_option("12")
+    source = spider.get_source()
+    month_dict, sum_day, sum_week = get_month_dict(source, sum_day, sum_week, str(year_need - 1), "12")
+    year_dict = dict(year_dict.items() + month_dict.items())
+    last_month_day = sum_day
+    last_month_week = sum_week
+
+    spider.select_year_option(str(year_need))
     for month in range(1, 13):
         # 月份格式化
         if month < 10:
@@ -141,15 +186,28 @@ def my_main():
 
         spider.select_month_option(month)
         source = spider.get_source()
-        month_dict, sum_day, sum_week = get_month_dict(source, sum_day, sum_week, year, month)
+        month_dict, sum_day, sum_week = get_month_dict(source, sum_day, sum_week, str(year_need),
+                                                       month, last_month_day, last_month_week)
         year_dict = dict(year_dict.items() + month_dict.items())
+        # break  # 测试
     print year_dict
+
+    # 跳转到需要获取那一年的后一年的1月，并获取数据
+    spider.select_year_option(str(year_need + 1))
+    spider.select_month_option("01")
+    source = spider.get_source()
+    month_dict, sum_day, sum_week = get_month_dict(source, sum_day, sum_week, str(year_need + 1), "01")
+    year_dict = dict(year_dict.items() + month_dict.items())
+
+    # 统计
     year_dict = count_next_workday(year_dict)
     year_dict = count_week_holiday(year_dict)
     print year_dict
-    col_name, col_data = save.format_data(year_dict)
-    save.write_calender(year + '年日历.csv', col_name, col_data)
+
+    col_name, col_data = save.format_data(year_dict, year_need)
+    save.write_calender(str(year_need) + '年日历.csv', col_name, col_data)
 
 
 if __name__ == '__main__':
-    my_main()
+    year = 2017
+    my_main(year)
--- save.py
+++ save.py
@@ -1,30 +1,38 @@
 # -*- coding: utf-8 -*-
 # Author: zhou
 # Time:  18-8-6 下午4:16
+import codecs
 import csv
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 
-def format_data(year_dict):
+def format_data(year_dict, year):
     """
     将获取到的数据转为写入格式
     :param year_dict:
+    :param year: 需要哪年的日历数据,int
     :return:
     """
-    col_name = ['日期', '今日是周几', '今日是本年的第几周', '今日是本年的第几天', '是否节假日', '本周的节假日有几天', '距离下一个工作日有几天']
+    col_name = ['日期', '今日是周几', '今日是本年的第几周', '今日是本年的第几天', '是否节假日', '本周的节假日有几天', '距离下一个工作日有几天', '节日名称']
     day_dict = {0: "星期天", 1: "星期一", 2: "星期二", 3: "星期三", 4: "星期四", 5: "星期五", 6: "星期六"}
     is_dict = {0: "否", 1: "是"}
     col_data = []
     for i in range(1, len(year_dict) + 1):
         day = year_dict[i]
-        day_list = []
-        day_list.append(day['date'])
-        day_list.append(day_dict[day['day']])
-        day_list.append(day['sum_week'])
-        day_list.append(day['sum_day'])
-        day_list.append(is_dict[day['is_holiday']])
-        day_list.append(day['week_holiday'])
-        day_list.append(day['next_workday'])
-        col_data.append(day_list)
+        if str(year) in day['date']:
+            day_list = []
+            day_list.append(day['date'])
+            day_list.append(day_dict[day['day']])
+            day_list.append(day['sum_week'])
+            day_list.append(day['sum_day'])
+            day_list.append(is_dict[day['is_holiday']])
+            day_list.append(day['week_holiday'])
+            day_list.append(day['next_workday'])
+            day_list.append(day['traditional_holiday'])
+            col_data.append(day_list)
+
     return col_name, col_data
 
 
@@ -37,6 +45,7 @@ def write_calender(filename, col_name, col_data):
     :return:
     """
     with open(filename, 'w') as f:
+        f.write(codecs.BOM_UTF8)
         writer = csv.writer(f)
         # 先写入列名
         writer.writerow(col_name)
